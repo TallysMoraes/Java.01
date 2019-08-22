0ee7aa8681e8602c7a0caa2f35852a3b8ea33066
tallys augusto <tallysmugetsu@gmail.com>
2019-08-22 19:33:41 -0300

    Exercicios de convercao

---
+++ exercicio.01/src/TestaVariaveis.java
@@ -0,0 +1,21 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		String nome = "tallys"; // String para valores de texto.
+		System.out.println(nome);
+		
+		int idade = 26; // int pra valores de numeros inteiros. 
+		System.out.println(idade);
+		
+		idade += (7 * 5 + 2); 
+		System.out.print(idade + "\n");
+		
+		System.out.println(nome + " tem " + idade + " anos.");
+		
+		double salario = 1270.50; // Para numeros quebrados
+		long valor = 64564654455845l; // Para numeros LONGOS
+		float isto = 3.45f;  // Para numeros desimais tbm
+	}
+
+}
