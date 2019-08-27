4a2edbd2edcb7483dcab18607c064de564e8ee4c
tallys augusto <tallysmugetsu@gmail.com>
2019-08-26 21:47:20 -0300

    Traticando if else

---
+++ exercicio.01/src/TesteCondicional.java
@@ -0,0 +1,19 @@
+
+public class TesteCondicional {
+
+	public static void main(String[] args) {
+		System.out.println("Testando Condicional");
+		int idade = 17;
+		int pais = 0;
+		
+		if(idade >= 18) {
+			System.out.println("Você é maior de Idade.");
+			System.out.println("Benvindo a boate Kiss! entrada liberada.");
+		}else {
+			if(pais>=2) {
+				
+			}System.out.println("Desculpe-me mas não poderar adentrar a balada Kiss, volte com teus pais.");
+		}
+		}
+
+}
--- exercicio.01/src/TesteConvercao.java
+++ exercicio.01/src/TesteConvercao.java
@@ -8,7 +8,7 @@ public class TesteConvercao {
 		short valorPequeno = 2131;
 		byte b = 127;
 		
-		System.out.printIn("Quantidade de filhos: " + filhos);
+		System.out.println("Quantidade de filhos: " + filhos);
 		
 		double salario = 1250.70;
 		System.out.printf("Salário: %.2f", salario);
