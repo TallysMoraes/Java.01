2ceaff79ca4c52f444a302f42e9df0a3915e8004
tallys augusto <tallysmugetsu@gmail.com>
2019-08-30 19:16:07 -0300

    acrecimo de arquivos pro teste

---
+++ exercicio.01/src/TesteDePooII_HoraExtra.java
@@ -0,0 +1,4 @@
+
+public class TesteDePooII_HoraExtra {
+	public static void main(String[] args) {}
+}
---
+++ exercicio.01/src/TesteDePooII_Nota.java
@@ -0,0 +1,9 @@
+
+public class TesteDePooII_Nota {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
