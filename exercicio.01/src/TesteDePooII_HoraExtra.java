518656b39ddf8c67d2df0f0d0336ef2d67d32248
tallys augusto <tallysmugetsu@gmail.com>
2019-09-02 21:34:23 -0300

    Questao dos teste de POOII

--- exercicio.01/src/TesteDePooII_HoraExtra.java
+++ exercicio.01/src/TesteDePooII_HoraExtra.java
@@ -1,4 +1,10 @@
 
 public class TesteDePooII_HoraExtra {
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		int horasTrab = 160 * 8;
+		int horasExtra = 20 * 12;
+		int salario = horasTrab + horasExtra;
+		
+		System.out.println("O salario do Felipe R$:" + horasTrab + "  \no 20H extras R$:" + horasExtra + " \n Total de R$:" + salario);
+	}
 }
--- exercicio.01/src/TesteDePooII_Nota.java
+++ exercicio.01/src/TesteDePooII_Nota.java
@@ -2,8 +2,17 @@
 public class TesteDePooII_Nota {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double p1 = 8.5;
+		double p2 = 5.0;
+		double p3 = 9.2;
+		
+		double notaFinal = p1 +p2 + p3;
+		
+		if(notaFinal < 7.0) {
+			System.out.printf("Sua media foi de: %.1f", (notaFinal /3) + " Infelizmente você foi reprovadpo");
+			
+		}else if(notaFinal >= 7.0) {
+			System.out.printf("Sua nota foi de: %.1f", (notaFinal /3) + " Parabens você foi aprovado!");
+			}
+		}
 	}
-
-}
