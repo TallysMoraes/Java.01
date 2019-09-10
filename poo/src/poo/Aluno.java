47e40764b310d8959e3f98bbe1b4d4d195a3bcec
tallys augusto <tallysmugetsu@gmail.com>
2019-09-09 21:50:29 -0300

    comeco de orienacao de bjeto em java

---
+++ poo/src/poo/Aluno.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class Aluno {
+	
+ int matricula;
+ String nome;
+ String sexo;
+ int idade;
+ int turma;
+ double nota1;
+ double nota2;
+}
---
+++ poo/src/poo/TestaAluno.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class TestaAluno {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		a1.nome = "tallys";
+		a1.idade = 26;
+		
+		
+		System.out.println("meu nome é: " + a1.nome);
+	}
+
+}
